2ef10de38130ff514c1f8053eafa524000397481
pedrobotolli <pedrobotolli.santos@gmail.com>
2023-09-14 16:01:33 -0300

    NLW Dia 3; partes do App.tsx viraram componentes novos; adicionada biblioteca AI para obter resposta do chatGPT em tempo real; adicionado ffmpeg para converter mp4 em mp3 utilizando WebAssembly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import { Github, Wand2 } from 'lucide-react'
 import { Button } from './components/ui/button'
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
 import { Label } from './components/ui/label'
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from './components/ui/select'
 import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input-form'
+import { PromptSelect } from './components/prompt-select'
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from './components/ui/select'
+import { useState } from 'react'
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string|null>(null)
+  
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
 
   return (
     <div className='min-h-screen flex flex-col'>
@@ -27,54 +51,25 @@ export function App() {
             <Textarea
               className='resize-none p-4 leading-relaxed'
               placeholder='Inclua o prompt para a IA...'
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className='resize-none p-4 leading-relaxed'
               placeholder='Resultado gerado pela IA...'
+              value={completion}
               readOnly
             />
           </div>
           <p className='text-sm text-muted-foreground'>Lembre-se: você pode utilizar a variável <code className='text-primary'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado</p>
         </div>
         <aside className='w-80 space-y-6'>
-          <form className='space-y-6'>
-            <label
-              htmlFor="video"
-              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/20'
-            >
-              <FileVideo className='w-4 h-4' />
-              Carregar video
-            </label>
-            <input type="file" id="video" accept="video/mp4" className='sr-only' />
-
-            <Separator />
-            <div className='space-y-1'>
-              <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
-              <Textarea
-                id='transcription_prompt'
-                className='h-20 leading-relaxed resize-none'
-                placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'
-              />
-            </div>
-            <Button type='submit' className='w-full'>
-              Carregar vídeo
-              <Upload className='w-4 h-4 ml-2' />
-            </Button>
-          </form>
+          <VideoInputForm onVideoUploaded={setVideoId} />
           <Separator />
-          <form className='space-y-6'>
+          <form className='space-y-6' onSubmit={handleSubmit}>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um Prompt..."></SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
-              <Separator />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
             <div className='space-y-2'>
               <Label>Modelo</Label>
@@ -86,6 +81,7 @@ export function App() {
                   <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
+
               <span className='block text-xs text-muted-foreground italic'>
                 Você poderá customizar essa opção em breve
               </span>
@@ -97,12 +93,14 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
               />
               <span className='block text-xs text-muted-foreground italic'>
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
               </span>
               <Separator />
-              <Button type='submit' className='w-full'>
+              <Button disabled={isLoading} type='submit' className='w-full'>
                 Executar
                 <Wand2 className='w-4 h-4 ml-2' />
               </Button>
